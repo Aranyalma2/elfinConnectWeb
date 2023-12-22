652c25ca04e487d45592ca538ad8a5ab65854796
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-12-22 17:51:50 +0100

    Version separated
    Separated file for version string.

--- middleware/renderMW.js
+++ middleware/renderMW.js
@@ -2,11 +2,13 @@
  * Using the template engine render the values into the template
  */
 
+const version = require('../route/version');
+
 const requireOption = require("./requireOption");
 
 module.exports = function (objectrepository, viewName) {
 	return function (req, res) {
-		res.locals.version = "Version 0.1.13 (pre-alpha)";
+		res.locals.version = version;
 		res.render(viewName);
 	};
 };
---
+++ route/version.js
@@ -0,0 +1,5 @@
+/*
+Version holder file
+*/
+
+module.exports = "Version 0.1.14 (dev)";
