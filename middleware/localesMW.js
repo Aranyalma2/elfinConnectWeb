0347a58405cdac424ab9a68267722fb6cb326cc1
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-01-28 11:45:42 +0100

    flags added
    Added flags for languages

--- middleware/localesMW.js
+++ middleware/localesMW.js
@@ -3,6 +3,5 @@ const langModel = require("../languages/locales");
 module.exports = function (objectrepository) {
 	return function (req, res, next) {
         res.locals.texts = langModel.getModel(req.cookies['lang']);
-
         return next();
     }}
