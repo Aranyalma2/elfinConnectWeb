6050ababffbaf85489a1540ea5b287344c901c0c
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-02-10 00:06:35 +0100

    upload folder create
    Create destination upload folder if it is not exists.

--- upload/upload-config.js
+++ upload/upload-config.js
@@ -1,16 +1,25 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 // Set up multer storage
 const desktopClientApps = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'static/downloads/desktopclient'); // Uploads will be stored in the 'uploads/' directory
+    cb(null, ensureFolderExists('./static/downloads/desktopclient')); // Uploads will be stored in the 'uploads/' directory
   },
   filename: (req, file, cb) => {
     const { name, extension } = splitByLastDot(file.originalname);
     cb(null, `${name}-${Date.now()}.${extension}`);
   },
 });
+
+function ensureFolderExists(folderPath) {
+    if (!fs.existsSync(folderPath)) {
+        // Folder doesn't exist, create it
+        fs.mkdirSync(folderPath, { recursive: true });
+    }
+    return folderPath
+}
 
 function splitByLastDot(inputString) {
   const lastDotIndex = inputString.lastIndexOf('.');
