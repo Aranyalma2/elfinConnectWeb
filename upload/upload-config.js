ccb7155ea54061b83016cc9b7a5b3a152afff2b0
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-02-09 23:55:38 +0100

    fix upload filename again

--- upload/upload-config.js
+++ upload/upload-config.js
@@ -7,8 +7,8 @@ const desktopClientApps = multer.diskStorage({
     cb(null, 'static/downloads/desktopclient'); // Uploads will be stored in the 'uploads/' directory
   },
   filename: (req, file, cb) => {
-    const { name, ext } = splitByLastDot(file.originalname)
-    cb(null, `${name}-${Date.now()}.${ext}`);
+    const { name, extension } = splitByLastDot(file.originalname);
+    cb(null, `${name}-${Date.now()}.${extension}`);
   },
 });
 
@@ -19,7 +19,7 @@ function splitByLastDot(inputString) {
     const firstPart = inputString.slice(0, lastDotIndex);
     const secondPart = inputString.slice(lastDotIndex + 1);
 
-    return [firstPart, secondPart];
+    return {name:firstPart, extension:secondPart};
   } else {
     // If no dot is found, return the entire string as the first part
     return [inputString];
