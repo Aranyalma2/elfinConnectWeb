1cb3fc276c580df3ea0a6267ba62a9f567d299e8
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-06-01 23:21:41 +0200

    Safe getDeviceMW

--- middleware/device/getDeviceMW.js
+++ middleware/device/getDeviceMW.js
@@ -8,6 +8,11 @@ module.exports = function (objectrepository) {
 	const DeviceDB = requireOption(objectrepository, "Device");
 
 	return function (req, res, next) {
+
+		if(!req.session.user.allDevices.includes(req.params.deviceid)){
+			return res.redirect("/");
+		}
+
 		DeviceDB.findOne({ _id: { $eq: req.params.deviceid } })
 			.then((device) => {
 				res.locals.device = device;
